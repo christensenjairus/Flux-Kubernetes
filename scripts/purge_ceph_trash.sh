9ab72d6db18e7a0b12116f7717f36f64d3e2c696
Jairus Christensen <christensenjairus@gmail.com>
2024-06-03 13:26:46 -0600

    Re-add separate pools for 2 and 3 replicas. Reduce storage requests for various workloads.

--- scripts/purge_ceph_trash.sh
+++ scripts/purge_ceph_trash.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#pools="ceph-blockpool-2 ceph-blockpool-3"
 pools="ceph-blockpool"
 
 # Get the current kubectl context
@@ -24,4 +25,20 @@ for pool in $pools; do
     echo "Processing $x..."
     kubectl rook-ceph rbd snap purge $pool/$x
   done
+
+#  ## Get all volume snapshots in a namespace
+#  snap_namespace=splunk
+#  for snap in $(kubectl get volumesnapshot -n $snap_namespace | egrep "k10|velero" | awk '{print $1}'); do kubectl get volumesnapshot -n $snap_namespace  $snap -o json | jq -r '.status.boundVolumeSnapshotContentName' >> $snap_namespace-snapnames.txt; done
+#
+#  ## Get snapshot contents for previous list of snapshots
+#  for snapcontent in $(cat $snap_namespace-snapnames.txt); do kubectl get volumesnapshotcontents $snapcontent -o json | jq -r '[.metadata.name,.spec.deletionPolicy,.status.snapshotHandle] | join(" ")'; done
+#
+#  ## Get ceph image IDs
+#  for snapcontent in $(cat $snap_namespace-snapnames.txt); do kubectl get volumesnapshotcontents $snapcontent -o json | jq -r '[.metadata.name,.spec.deletionPolicy,.status.snapshotHandle] | join(" ")' | tail -c 37 >> $snap_namespace-snaphandle-names.txt ; done
+#
+#  ## Patch snapcontent to allow delete
+#  for snapcontent in $(cat $snap_namespace-snapnames.txt); do kubectl patch volumesnapshotcontents $snapcontent --type=merge -p '{"spec":{"deletionPolicy": "Delete"}}'; done
+#
+#  ## Delete VolumeSnapshot which triggers VolumeSnapshotContent deletion
+#  for snap in $(kubectl get volumesnapshot -n $snap_namespace | egrep "k10|velero" | awk '{print $1}'); do kubectl delete volumesnapshot -n $snap_namespace $snap; done
 done
