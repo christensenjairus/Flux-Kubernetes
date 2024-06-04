8400a8489beb14ea6c7ca1bbb885806cb394d92c
Jairus Christensen <christensenjairus@gmail.com>
2024-06-04 10:27:49 -0600

    Rearrange scripts folder. Rename homarr pvcs. Add migrate folder to scripts.

--- apps/base/authelia/set_db_password.sh
+++ apps/base/authelia/set_db_password.sh
@@ -19,4 +19,4 @@ echo "Continuing with context $current_context..."
 DB_USERNAME=$(op item get "Authelia DB Creds" --vault "HomeLab K8S" --format json | jq '.fields[0].value' | tr -d '"')
 DB_PASSWORD=$(op item get "Authelia DB Creds" --vault "HomeLab K8S" --format json | jq '.fields[1].value' | tr -d '"')
 
-python3 ../../../scripts/set_postgrescluster_pw.py "$DB_USERNAME" "$DB_PASSWORD" authelia authelia-db-pguser-authelia
+python3 ../../../scripts/helpers/set_postgrescluster_pw.py "$DB_USERNAME" "$DB_PASSWORD" authelia authelia-db-pguser-authelia
