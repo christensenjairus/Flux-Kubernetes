b8f7c52d29ab4e7a3ee5ed712bc014c5f07fb437
Jairus Christensen <christensenjairus@gmail.com>
2024-10-16 09:06:24 -0600

    Add search scripts for modsecurity waf logs

--- tools/waf/search-waf-blocks.sh
+++ tools/waf/search-waf-blocks.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 NAMESPACE="ingress-nginx-public"
-LOG_DIR="/audit-logs/20241016"
+BASE_LOG_DIR="/audit-logs"
+LOG_DIR="${BASE_LOG_DIR}/${1:-}"  # Append the passed argument as a suffix or use just the base log directory if no argument is passed
 POD_NAME=$(kubectl get pods -n $NAMESPACE -l app.kubernetes.io/name=ingress-nginx --output=jsonpath='{.items[0].metadata.name}')
+COUNT=0
 
 # Ensure we can exec into the ingress controller pod
 if [[ -z "$POD_NAME" ]]; then
@@ -10,41 +12,31 @@ if [[ -z "$POD_NAME" ]]; then
   exit 1
 fi
 
-echo "Execing into pod $POD_NAME..."
+# Check if at least one search term is provided
+if [[ -z "$2" ]]; then
+  echo "Usage: $0 <search-term>"
+  echo "Example: $0 'christensencloud.us|cyber-engine.com|siteclouds.net'"
+  exit 1
+fi
 
-# Command to execute inside the ingress controller to list ModSecurity logs
-kubectl exec -n $NAMESPACE $POD_NAME -- bash -c "grep -rE 'christensencloud.us|cyber-engine.com|siteclouds.net' $LOG_DIR" | \
+SEARCH_TERM=$1
+
+echo "Execing into pod $POD_NAME and searching for '$SEARCH_TERM'..."
+
+# Command to execute inside the ingress controller to list ModSecurity logs based on the search term
+kubectl exec -n $NAMESPACE $POD_NAME -- bash -c "find $LOG_DIR -type f | sort | xargs grep -rE '$2'" | \
 sed 's/^[^:]*://g' | while read -r line; do
     # Ensure that only valid JSON entries are passed to jq
     if echo "$line" | grep -q '{.*}'; then
         # Process the logs with jq locally
-        echo "$line" | jq -c '
-          {
-            timestamp: .transaction.time_stamp,
-            rule_id: (.transaction.messages[].details.ruleId // "Unknown"),
-            severity: (.transaction.messages[].details.severity // "Unknown"),
-            maturity: (.transaction.messages[].details.maturity // "Unknown"),
-            accuracy: (.transaction.messages[].details.accuracy // "Unknown"),
-            client_ip: .transaction.client_ip,
-            host: .transaction.request.headers.host,
-            uri: .transaction.request.uri,
-            data: (.transaction.messages[].details.data // "Unknown")
-          }
-        '
+        COUNT=$((COUNT + 1))
+        echo "======================================== $COUNT ================================================="
+        echo "$line" | jq 'del(
+          .transaction.response.body,
+          .transaction.response.headers,
+          .transaction.server_id,
+          .transaction.producer.modsecurity,
+          .transaction.producer.connector
+        )'
     fi
-done #| awk '
-#    BEGIN {
-#      # Print column headers
-#      printf("%-10s %-15s %-40s %-30s\n", "Count", "Rule ID", "Source IP", "URI")
-#    }
-#    {
-#      count[$2]++;
-#      ruleId[$2]=$2;
-#      client_ip[$2]=$3;
-#      uri[$2]=$4;
-#    }
-#    END {
-#      for (id in count) {
-#        printf("%-10s %-15s %-40s %-30s\n", count[id], ruleId[id], client_ip[id], uri[id])
-#      }
-#    }'
+done
