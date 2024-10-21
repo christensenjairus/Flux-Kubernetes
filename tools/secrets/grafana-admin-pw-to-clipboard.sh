2f88482fa9e3a7adce8549d1ae5ddc03e11e7874
Jairus Christensen <christensenjairus@gmail.com>
2024-10-21 01:18:31 -0600

    Fix various dashboards to use the grafana-operator.

--- tools/secrets/grafana-admin-pw-to-clipboard.sh
+++ tools/secrets/grafana-admin-pw-to-clipboard.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl get secret -n monitoring grafana-admin-credentials -ojsonpath='{.data.GF_SECURITY_ADMIN_PASSWORD}' | base64 -d | pbcopy
+kubectl get secret -n grafana grafana-admin-credentials -ojsonpath='{.data.GF_SECURITY_ADMIN_PASSWORD}' | base64 -d | pbcopy
